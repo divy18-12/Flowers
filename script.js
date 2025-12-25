65a3702834d2dacaab8caa11e1bba07b6b14b418
divy18-12 <divyansh.kriss@gmail.com>
2025-12-25 20:23:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
+
+    document.getElementById("cornerMessage").classList.add("show");
+
     clearTimeout(c);
   }, 1000);
 };
